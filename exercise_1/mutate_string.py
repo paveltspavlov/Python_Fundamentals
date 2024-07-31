563a6ea63b2126dc4275e1ca8e2d990a967865b1
paveltspavlov <pavel_ts_pavlov@outlook.com>
2024-07-31 20:52:02 +0300

    exercise 1 task 10 complete

--- exercise_1/mutate_string.py
+++ exercise_1/mutate_string.py
@@ -2,15 +2,14 @@ string_one = input()
 string_two = input()
 buff = list(string_one)
 buff2 = list(string_two)
-delimiter_comma = ","
+delimiter_comma = ""
 printable = ""
 i = 0
 
-while i < 4:
+for i in range (len(string_one)):
     if buff[i] != buff2[i]:
         buff[i] = buff2[i]
         printable = delimiter_comma.join(buff)
+        output = printable.replace(",", "")
         print(printable)
-        i += 1
-    else:
-        continue
+    i += 1
