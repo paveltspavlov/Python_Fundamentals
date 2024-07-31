5c250a9c2ac28d77eb0fc8b0f20f6fc25cc4750a
paveltspavlov <pavel_ts_pavlov@outlook.com>
2024-07-31 22:10:22 +0300

    exercise 1 task 11 Completed

--- exercise_1/easter_bread.py
+++ exercise_1/easter_bread.py
@@ -4,4 +4,16 @@ eggs_price = flour_price * 0.75
 milk_price = flour_price * 1.25
 
 loaf_price = flour_price + eggs_price + (milk_price / 4)
+loaf_counter = 0
+colored_eggs = 0
 
+while budget >= loaf_price:
+    budget -= loaf_price
+    if budget <= 0:
+        break
+    loaf_counter += 1
+    colored_eggs += 3
+    if loaf_counter % 3 == 0:
+        colored_eggs -= (loaf_counter - 2)
+
+print(f"You made {loaf_counter} loaves of Easter bread! Now you have {colored_eggs} eggs and {budget:.2f}BGN left.")
