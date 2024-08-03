2d6009698698a8faeba877e037b5bb49b570dca7
pavel <pavel_ts_pavlov@outlook.com>
2024-08-03 16:32:14 +0300

    exercise 1 task 12 done

--- exercise_1/christmas_spirit.py
+++ exercise_1/christmas_spirit.py
@@ -14,30 +14,29 @@ total_points = 0
 
 for i in range(1, days+1, +1):
 
-    if i % 2 == 0:
-        total_price += ornament_price
-        total_points += ornament_points
-    if i % 3 == 0:
-        total_price += tree_skirt_price + tree_garland_price
-        total_points += tree_skirt_points + tree_garland_points
-    if i % 5 == 0:
-        total_price += tree_lights_price
-        total_points += tree_lights_points
-    if i % 15 == 0:
-        total_points += 30
-        total_price += tree_skirt_price + tree_garland_price + tree_lights_price
+    if i % 11 == 0:
+        quantity_of_decorations += 2
 
     if i % 10 == 0:
         total_points -= 20
         total_price += tree_skirt_price + tree_garland_price + tree_lights_price
 
-    if i % 11 == 0:
-        quantity_of_decorations += 2
+        if i % 10 == 0 and days == i:
+            total_points -= 30
+    if i % 5 == 0:
+        total_price += quantity_of_decorations * tree_lights_price
+        total_points += tree_lights_points
 
-    if i == 10:
-        total_points -= 30
+    if i % 15 == 0:
+        total_points += 30
 
-    quantity_of_decorations -= 1
+    if i % 3 == 0:
+        total_price += quantity_of_decorations * (tree_skirt_price + tree_garland_price)
+        total_points += tree_skirt_points + tree_garland_points
+
+    if i % 2 == 0:
+        total_price += quantity_of_decorations * ornament_price
+        total_points += ornament_points
 
 print(f"Total cost: {total_price}")
 print(f"Total spirit: {total_points}")
