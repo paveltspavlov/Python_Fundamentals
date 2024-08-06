35c7b7a916eaef601c5383a9cc05cd498e31ae4d
paveltspavlov <pavel_ts_pavlov@outlook.com>
2024-08-06 08:47:59 +0300

    List Lab Done

--- lists_basics_lab/numbers_filter.py
+++ lists_basics_lab/numbers_filter.py
@@ -1,8 +1,23 @@
 numbers_count = int(input())
 numbers_list = []
 buff = []
+buff_2 = []
 
 for i in range(numbers_count):
     buff = input()
     numbers_list.append(buff)
 
+command = input()
+
+if command == "even":
+    buff_2 = [int(num) for num in numbers_list if int(num) % 2 == 0]
+    print(buff_2)
+elif command == "odd":
+    buff_2 = [int(num) for num in numbers_list if int(num) % 2 != 0]
+    print(buff_2)
+elif command == "negative":
+    buff_2 = [int(num) for num in numbers_list if int(num) < 0]
+    print(buff_2)
+else:
+    buff_2 = [int(num) for num in numbers_list if int(num) > 0]
+    print(buff_2)
