c53bf5faa854c0dadcb5e6977f36b0f412a1f7e9
pavelpavlov <pavel_ts_pavlov@outlook.com>
2024-08-15 17:09:43 +0300

    Lists Basics - Exercise WiP

--- list_basics_exercise/football_cards.py
+++ list_basics_exercise/football_cards.py
@@ -1,2 +1,16 @@
-input_list = [item.strip() for item in input().split(" ")]
+input_list = [item for item in input().split(" ")]
+card = []
+output_a = 0
+output_b = 0
 
+for i in range(0, len(input_list)):
+    card[i] = input_list[i].split("-")
+
+    if card[i] == "A":
+        output_a += 1
+    elif card[i] == "B":
+        output_b += 1
+
+    i += 1
+
+print(f"Team A - {11 - output_a}; Team B - {11 - output_b}")
