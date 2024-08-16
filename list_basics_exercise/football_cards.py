f875403a232b3078bdea1ad63de4c7104655b922
paveltspavlov <pavel_ts_pavlov@outlook.com>
2024-08-16 06:43:38 +0300

    List Exercise WiP

--- list_basics_exercise/football_cards.py
+++ list_basics_exercise/football_cards.py
@@ -1,16 +1,22 @@
 input_list = [item for item in input().split(" ")]
-card = []
 output_a = 0
 output_b = 0
+unique_cards_a = set()
+unique_cards_b = set()
 
-for i in range(0, len(input_list)):
-    card[i] = input_list[i].split("-")
+for item in input_list:
+    card = item.split("-")
 
-    if card[i] == "A":
+    if card[0] == "A":
+        unique_cards_a.add(card[1])
         output_a += 1
-    elif card[i] == "B":
+    elif card[0] == "B":
+        unique_cards_b.add(card[1])
         output_b += 1
 
-    i += 1
+if (11 - len(unique_cards_a)) > 7 or (11 - len(unique_cards_b)) > 7:
+    print(f"Team A - {11 - len(unique_cards_a)}; Team B - {11 - len(unique_cards_b)}")
 
-print(f"Team A - {11 - output_a}; Team B - {11 - output_b}")
+elif (11 - len(unique_cards_a)) <= 7 or (11 - len(unique_cards_b)) <= 7:
+    print(f"Team A - {11 - len(unique_cards_a)}; Team B - {11 - len(unique_cards_b)}")
+    print("Game was terminated")
